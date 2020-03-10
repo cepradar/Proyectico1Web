8cb59c60fd54298de3f0727b470188bc44520bd1
Carlos Prada <carlosprada95@gmail.com>
2020-03-10 17:44:54 -0500

    falta organizar el guardar

--- logica.js
+++ logica.js
@@ -37,7 +37,7 @@ function agregarUsuario() {
                                     + "<td>" + estatura.val() + "</td>"
                                     + "<td>" + edad.val() + "</td>"
                                     + "<td>" + localidad.val() + "</td>"
-            + "<td><a href'#' name='edit' class='editar button'>Editar</a>" + "<a href='#' class='eliminar button'>Eliminar</a></td>" +
+            + "<td><button class='btn btn-primary editar'>Editar</button> &nbsp" + "<button class='btn btn-primary eliminar'>Eliminar</button></td>" +
             "</tr>");
 
         nSocio.val('');
@@ -53,38 +53,60 @@ function editarUsuario() {
     $('#u-tabla').on('click', '.editar', function (e) {
         e.preventDefault();
         var tr = $(this).closest('tr');
-        var tdNombre = tr.children("td:nth-child(1)");
-        var tdApellido = tr.children("td:nth-child(2)");
-        var tdSexo = tr.children("td:nth-child(3)");
-        var tdOpciones = tr.children("td:nth-child(4)");
-        var vnombre = tdNombre.html();
-        tdNombre.html("<input type='text' id='txtNombre' value=''" + vnombre + "'/>");
-        var vapellidos = tdApellidos.html();
-        tdApellido.html("<input type='text' id='txtApellido' value=''" + vapellidos + "'/>");
-        tdOpciones.html("<a href='#' class='guardar button'>Guardar</a>" +
-            "<a href='#' class='eliminar button'>Eliminar</a>")
+        var tdNSocio = tr.children("td:nth-child(1)");
+        var tdNombre = tr.children("td:nth-child(2)");
+        var tdEstatura = tr.children("td:nth-child(3)");
+        var tdEdad = tr.children("td:nth-child(4)");
+        var tdLocalidad = tr.children("td:nth-child(5)");
+        var tdOpciones = tr.children("td:nth-child(6)");
 
+        var vNSocio = tdNSocio.html(); 
+        tdNSocio.html("<input type='text' id='txtNSocio' value='" + vNSocio + "'/>");
+        
+        var vNombre = tdNombre.html();
+        tdNombre.html("<input type='text' id='txtNombre' value='" + vNombre + "'/>");
+        
+        var vEstatura = tdEstatura.html();
+        tdEstatura.html("<input type='text' id='txtEstatura' value='" + vEstatura + "'/>");
+        
+        var vEdad = tdEdad.html();
+        tdEdad.html("<input type='text' id='txtEdad' value='"+ vEdad + "'/>");
+        
+        var vLocalidad = tdLocalidad.html();
+        tdLocalidad.html("<input type='text' id='txtLocalidad' value='"+ vLocalidad + "'/>");
+
+        tdOpciones.html("<button class='btn btn-primary guardar'>Guardar </button>" + "<button class='btn btn-primary eliminar'>Eliminar</button>");
     });
 }
 
 function guardarEdicionUsuario() {
     $('#u-tabla').on('click','.guardar',function(e){
         e.preventDefault();
-
+        alert("hola");
         var tr= $(this).closest('tr');
-        var tdNombre = tr.children("td:nth-child(1)");
-        var tdApellidos = tr.children("td:nth-child(2)");
-        var tdSexo =tr.children("td:nth-child(3)");
-        var tdOpciones = tr.children("td:nth-child(4)");
+        var tdNSocio = tr.children("td:nth-child(1")
+        var tdNombre = tr.children("td:nth-child(2)");
+        var tdEstatura = tr.children("td:nth-child(3)");
+        var tdEdad = tr.children("td:nth-child(4)");
+        var tdLocalidad = tr.children("td:nth-child(5)")
+        var tdOpciones = tr.children("td:nth-child(6)");
 
-        nuevoNombre = tdNombre.children("input[type=text]").val();
+        var nuevoNSocio = tdNSocio.children("input[type=text]").val();
+        tdNSocio.html(nuevoNSocio);
+
+        var nuevoNombre = tdNombre.children("input[type=text]").val();
         tdNombre.html(nuevoNombre);
 
-        nuevoApellidos = tdApellidos.children("input[type=text]").val();
-        tdApellidos.html(nueApellidos);
+        var nuevaEstatura = tdEstatura.children("input[type=text]").val();
+        tdEstatura.html(nuevaEstatura);
 
-        tdOpciones.html( "<a  href='#' class='editar button'> Editar</a>"+  
-                         "<a href='#' class='Eliminar button'> Eliminar</a>");
+        var nuevaEdad = tdEdad.children("input[type=text]").val();
+        tdEdad.html(nuevaEdad);
+
+        var nuevaLocalidad = tdLocalidad.children("input[type=text]").val();
+        tdLocalidad.html(nuevaLocalidad);
+
+        tdOpciones.html( "<button class='btn btn-primary editar'>Editar</button>" + "<button class='btn btn-primary eliminar'>Eliminar</button></td>");
     });
     
 }    
